9af1e0c9f719c12a8e4dc29e033583488bd597f5
taslima97 <antora.taslima97@gmail.com>
2023-05-19 09:26:06 +0600

    carocol

--- src/page/Home/Banner/Banner.jsx
+++ src/page/Home/Banner/Banner.jsx
@@ -1,9 +1,83 @@
-import './Banner.css'
+import {FiArrowUpRight} from 'react-icons/fi';
 
 const Banner = () => {
     return (
-        <div className="banner">
-           <h2>Hello banner</h2> 
+        <div className="carousel w-full h-[600px]">
+            <div id="slide1" className="carousel-item relative w-full">
+                <img src='' className="w-full rounded-xl" />
+                <div className="absolute rounded-xl flex items-center top-0 bottom-0 bg-gradient-to-r from-[#f1b3b3] to-[rgba(21, 21, 21, 0)]">
+                    <div className='text-white space-y-7 mx-auto  text-center'>
+                        <h3 className='text-6xl font-bold '>It is good
+                            tea time at The Tea House</h3>
+                        <p>Tea and Botanical Solutions Supplier Give Optimum Satisfaction To Your Taste Buds.</p>
+                        <div>
+                            <button className="btn btn-primary mr-4 bg-gradient-to-r from-[#e76767] to-[rgba(21, 21, 21, 0)]">Explore More<FiArrowUpRight></FiArrowUpRight></button>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex justify-between items-center transform -translate-y-1/2  left-10 right-10 top-1/2 bottom-0">
+                    <a href="#slide4" className="btn btn-circle mr-5">❮</a>
+                    <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+                <img src='' className="w-full rounded-xl" />
+                <div className="absolute rounded-xl flex items-center top-0 bottom-0 bg-gradient-to-r from-[#f1b3b3] to-[rgba(21, 21, 21, 0)]">
+                    <div className='text-white space-y-7 mx-auto  text-center'>
+                        <h3 className='text-6xl font-bold '>It is good
+                            tea time at The Tea House</h3>
+                        <p>Tea and Botanical Solutions Supplier Give Optimum Satisfaction To Your Taste Buds.</p>
+                        <div>
+                            <button className="btn btn-primary mr-4 bg-gradient-to-r from-[#e76767] to-[rgba(21, 21, 21, 0)]">Explore More<FiArrowUpRight></FiArrowUpRight></button>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex justify-between text-center transform -translate-y-1/2 top-1/2 left-5 right-5 bottom-0">
+          <a href="#slide1" className="btn btn-circle mr-5">❮</a>
+          <a href="#slide3" className="btn btn-circle">❯</a>
+        </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+                <img src='' className="w-full rounded-xl" />
+                <div className="absolute rounded-xl flex items-center top-0 bottom-0 bg-gradient-to-r from-[#f1b3b3] to-[rgba(21, 21, 21, 0)]">
+                    <div className='text-white space-y-7 mx-auto  text-center'>
+                        <h3 className='text-6xl font-bold '>It is good
+                            tea time at The Tea House</h3>
+                        <p>Tea and Botanical Solutions Supplier Give Optimum Satisfaction To Your Taste Buds.</p>
+                        <div>
+                            <button className="btn btn-primary mr-4 bg-gradient-to-r from-[#e76767] to-[rgba(21, 21, 21, 0)]">Explore More<FiArrowUpRight></FiArrowUpRight></button>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex justify-between items-center transform -translate-y-1/2  left-10 right-10 top-1/2 bottom-0">
+                    <a href="#slide2" className="btn btn-circle mr-5">❮</a>
+                    <a href="#slide4" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full">
+                <img src='' className="w-full rounded-xl" />
+                <div className="absolute rounded-xl flex items-center top-0 bottom-0 bg-gradient-to-r from-[#f1b3b3] to-[rgba(21, 21, 21, 0)]">
+                    <div className='text-white space-y-7 mx-auto  text-center'>
+                        <h3 className='text-6xl font-bold '>It is good
+                            tea time at The Tea House</h3>
+                        <p>Tea and Botanical Solutions Supplier Give Optimum Satisfaction To Your Taste Buds.</p>
+                        <div>
+                            <button className="btn btn-primary mr-4 bg-gradient-to-r from-[#e76767] to-[rgba(21, 21, 21, 0)]">Explore More<FiArrowUpRight></FiArrowUpRight></button>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex justify-between items-center transform -translate-y-1/2  left-10 right-10 top-1/2 bottom-0">
+                    <a href="#slide3" className="btn btn-circle mr-5">❮</a>
+                    <a href="#slide1" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            
+                
+            
         </div>
     );
 };

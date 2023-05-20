afa75dbec44258e5d8ead7b6fd061eff74e81f7f
taslima97 <antora.taslima97@gmail.com>
2023-05-20 16:54:43 +0600

    home page done

--- src/page/Home/Home/Home.jsx
+++ src/page/Home/Home/Home.jsx
@@ -3,8 +3,7 @@ import About from "../About/About";
 import Banner from "../Banner/Banner";
 import Gallery from "../Gallery/Gallery";
 import Tab from "../Tab/Tab";
-
-
+import img from '../../../assets/images/two-new.jpg'
 const Home = () => {
 
     return (
@@ -13,7 +12,13 @@ const Home = () => {
             <Gallery></Gallery>
             <Tab></Tab>
             <About></About>
-           <h2 className="text-6xl">This is home</h2> 
+            <div className="relative my-24 mx-auto w-1/2 bg-gradient-to-r from-[#f1b3b3] to-[rgba(21, 21, 21, 0)]">
+                <img className="h-48 w-full" src={img} alt="" />
+                <div className="absolute top-0 bottom-0">
+                    <h2 className="text-5xl font-bold">Awlays keep a positive <br /> mindset</h2>
+
+                </div>
+            </div>
         </div>
     );
 };
